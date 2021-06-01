9f46f779253f39f7268c64cd58137444a2a7e376
Юрок <@->
2021-06-01 21:01:00 +0300

    Добавьте файлы проекта.

---
+++ Test/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Persona
+    {
+        private String name;
+        private String surname;
+
+
+        void setName(String name)
+        {
+            this.name = name;
+        }
+
+        String getName()
+        {
+            return this.name;
+        }
+
+        void setSurname(String surname)
+        {
+            this.surname = surname;
+        }
+
+        String getSurname()
+        {
+            return surname;
+        }
+    }
+}
